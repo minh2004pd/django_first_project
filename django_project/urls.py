37ed8bc8f5ebd622019ca5b1bd0f320fa3d98c73
Doan Duc Minh <114096378+minh2004pd@users.noreply.github.com>
2023-05-09 10:55:22 +0700

    update profile

--- django_project/urls.py
+++ django_project/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as users_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +29,6 @@ urlpatterns = [
     path('profile/', users_views.profile, name = 'profile'),
     path('', include('blog.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
