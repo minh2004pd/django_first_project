16ef4417f5b2e37008a46119fcf2614ad98d4cc7
Doan Duc Minh <114096378+minh2004pd@users.noreply.github.com>
2023-05-25 22:53:51 +0700

    post image

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -2,15 +2,29 @@ from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from PIL import Image
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default = timezone.now())
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    imagePost = models.ImageField(default='default.jpg',upload_to='profile_pics')
     
     def __str__(self):
         return self.title
     
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk':self.pk})
+    
+    def save(self,*args, **kwargs):
+        super().save(*args, **kwargs)
+
+        imagePost = Image.open(self.imagePost.path)
+
+        if imagePost.height > 300 and imagePost.width > 300:
+            output_size = (100,100)
+            imagePost.thumbnail(output_size)
+            imagePost.save(self.imagePost.path)
+    
+    
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
+from .form import PostForm
 
 def home(request):
     context= {
@@ -37,7 +38,7 @@ class PostDetailView(DetailView):
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    fields=['title','content']
+    form_class = PostForm
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -45,7 +46,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
-    fields=['title','content']
+    form_class = PostForm
 
     def form_valid(self,form):
         form.instance.author = self.request.user
